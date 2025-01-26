82b192da29cf31c7ddec9507be614f125730899e
Rachana Pandey <rachuu244@gmail.com>
2025-01-26 14:50:49 +0545

    feat: added router

--- OneDrive/Desktop/react-real-pro/my-new-app/src/App.js
+++ OneDrive/Desktop/react-real-pro/my-new-app/src/App.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import Navbar from './components/NavBar';
-import Movie from './components/Movie';
-import Title from './components/Title';
-import Search from './components/Search';
-import Slider from './components/Slider';
-import {createBrowserRoute, createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import MovieDet from './components/MovieDetails';
-import NotFound from './components/NotFound';
-import MovieDetail from './components/MovieDetails';
+import React from "react";
+import Navbar from "./components/NavBar";
+import Movie from "./components/Movie";
+import Title from "./components/Title";
+import Search from "./components/Search";
+import Slider from "./components/Slider";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import MovieDet from "./components/MovieDetails";
+import NotFound from "./components/NotFound";
+import MovieDetail from "./components/MovieDetails";
 
 function App() {
-  const myRoutes = createBrowserRouter([
-    {path: "/", element: <Home/> },
-    {path: "/about", element: <About/>},
-    {path: "/contact", element: <Contact/>},
-    {path: "/movie-details/:id?", element: <MovieDetail/>},
-    {path: "*", element: <NotFound/>},
-    
-  ])
+  const myRoutes = createHashRouter([
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "/contact", element: <Contact /> },
+    { path: "/movie-details/:id?", element: <MovieDetail /> },
+    { path: "*", element: <NotFound /> },
+  ]);
   return (
     <div>
-      
-      <RouterProvider router={myRoutes}/>
-
+      <RouterProvider router={myRoutes} />
     </div>
   );
 }
 
 export default App;
-
-
