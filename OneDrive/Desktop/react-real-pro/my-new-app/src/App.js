73f669064ffd89158d83152be6fcf7632b14e36b
Rachana Pandey <rachuu244@gmail.com>
2025-01-26 14:56:22 +0545

    Changes

--- OneDrive/Desktop/react-real-pro/my-new-app/src/App.js
+++ OneDrive/Desktop/react-real-pro/my-new-app/src/App.js
@@ -1,28 +1,25 @@
 import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
-import Movie from "./components/Movie";
-import Title from "./components/Title";
-import Search from "./components/Search";
-import Slider from "./components/Slider";
-import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import MovieDet from "./components/MovieDetails";
+import MovieDetails from "./components/MovieDetails";
 import NotFound from "./components/NotFound";
-import MovieDetail from "./components/MovieDetails";
 
 function App() {
-  const myRoutes = createHashRouter([
-    { path: "/", element: <Home /> },
-    { path: "/about", element: <About /> },
-    { path: "/contact", element: <Contact /> },
-    { path: "/movie-details/:id?", element: <MovieDetail /> },
-    { path: "*", element: <NotFound /> },
-  ]);
   return (
     <div>
-      <RouterProvider router={myRoutes} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/movie-details/:id" element={<MovieDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
